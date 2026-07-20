b42f4f3f74c6db19967ee2bc14d1c188daa9dae3
chan.jeon <32017833+cheonvi1004@users.noreply.github.com>
2026-07-20 15:51:10 +0900

    Delete src/main directory

--- src/main/java/com/yourorg/facility/config/FacilityCallProperties.java
+++
@@ -1,9 +0,0 @@
-package com.yourorg.facility.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "facility.call")
-public record FacilityCallProperties(
-        int accessTimeoutSeconds
-) {
-}
--- src/main/java/com/yourorg/facility/config/FacilityCommandProperties.java
+++
@@ -1,9 +0,0 @@
-package com.yourorg.facility.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "facility.command")
-public record FacilityCommandProperties(
-        int defaultTimeoutSeconds
-) {
-}
