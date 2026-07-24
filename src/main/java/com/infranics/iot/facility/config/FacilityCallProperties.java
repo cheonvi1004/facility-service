a4fc884a3daf21fc701c5fb74086dda65cd8ce59
chan.jeon <32017833+cheonvi1004@users.noreply.github.com>
2026-07-24 17:58:51 +0900

    Delete src/main directory

--- src/main/java/com/infranics/iot/facility/config/FacilityCallProperties.java
+++
@@ -1,9 +0,0 @@
-package com.infranics.iot.facility.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "facility.call")
-public record FacilityCallProperties(
-        int accessTimeoutSeconds
-) {
-}
--- src/main/java/com/infranics/iot/facility/config/FacilityCommandProperties.java
+++
@@ -1,9 +0,0 @@
-package com.infranics.iot.facility.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "facility.command")
-public record FacilityCommandProperties(
-        int defaultTimeoutSeconds
-) {
-}
